6310db7b7afcf8211577b308d23c3744f31e4f55
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2016-08-26 15:25:59 +0530

    Added Categories to Article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :articles
+  has_many :categories
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
